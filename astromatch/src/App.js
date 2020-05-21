09ff691d2868ff0b274e947a9cad6efcd807eb22
Wendel de Lima <wendel.delima94@gmail.com>
2020-05-21 18:45:43 -0300

    Lógica concluida

--- astromatch/src/App.js
+++ astromatch/src/App.js
@@ -1,82 +1,14 @@
-import React, { Component, useState } from "react";
-import styled from "styled-components";
-// import BotaoX from "@material-ui/icons/Clear";
-// import BotaoCoracao from "@material-ui/icons/FavoriteBorder";
-// import Button from "@material-ui/core/Button";
-import { createMuiTheme, MuiThemeProvider } from "@material-ui/core";
-import { Botoes } from './Components/Botoes.js';
-import axios from "axios";
+import React from 'react'
+import Main from './Components/Main/Main'
+import ResetButton from './Components/ResetButton/ResetButton'
 
-
-const MyTheme = createMuiTheme({
-  palette: {
-    primary: {
-      main: "#43a047",
-    },
-    secondary: {
-      main: "#f44336",
-    },
-  },
-});
-
-const PalavraAstro = styled.span`
-  color: #ffc107;
-  font-size: 40px;
-`;
-const PalavraMatch = styled.span`
-  color: #5d4037;
-  font-size: 40px;
-`;
-// const Container = styled.div`
-// width: 400px;
-// height: 500px;
-// background-color: #bdbdbd ;
-// display:flex;
-// justify-content: center`;
-
-
-const ContainerPrincipal = styled.div`
-  position: absolute;
-  left: 30%;
-  right: 30%;
-  border: 2px solid black;
-  width: 450px;
-  height: 500px;
-  background-color: #eeeeee;
-  display: flex;
-  justify-content: center;
-  box-shadow: 1px 2px 5px 0px rgba(50, 50, 50, 0.75);
-`;
-
-
-function App() {
-  const [listaUsuarios, setListaUsuarios] = useState([])
-  
-  const baseUrl = "https://us-central1-missao-newton.cloudfunctions.net/astroMatch/:aluno/person"
-
-  getProfile = () => {
-    axios
-        .get(baseUrl)
-        .then(response => {
-          setListaUsuarios({listaUsuarios: response.data.profile})
-        })
-        .catch(err => {
-          console.log(err)
-        })
-  }
-
-
-  return (
-    <MuiThemeProvider theme={MyTheme}>
-        <ContainerPrincipal>
-        {/* <Container> */}
-           <PalavraAstro>astro</PalavraAstro>
-           <PalavraMatch>match</PalavraMatch>
-           <Botoes />
-        {/* </Container> */}
-       </ContainerPrincipal>
-    </MuiThemeProvider>
-  );
+function App(){
+  return(
+    <div>
+      <Main/>
+      <ResetButton/>
+    </div>
+  )
 }
 
-export default App;
+export default App
