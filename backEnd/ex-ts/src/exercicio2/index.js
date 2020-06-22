dd24892c8fb4406080b31a3542ea2f374bc44302
Wendel de Lima <wendel.delima94@gmail.com>
2020-06-22 20:23:23 -0300

    Atualizando o exercicio 2

---
+++ backEnd/ex-ts/src/exercicio2/index.js
@@ -0,0 +1,20 @@
+function adicao(a, b) {
+    return a + b;
+}
+var resultAdicao = adicao(7, 3);
+console.log(resultAdicao);
+function subtracao(a, b) {
+    return a - b;
+}
+var resultSubtracao = subtracao(9, 4);
+console.log(resultSubtracao);
+function multiplicacao(a, b) {
+    return a * b;
+}
+var resultMultiplicacao = multiplicacao(3, 5);
+console.log(resultMultiplicacao);
+function maiorQue(a, b) {
+    return a > b ? a : b;
+}
+var resultMaiorQue = maiorQue(12, 5);
+console.log(resultMaiorQue);
---
+++ backEnd/ex-ts/src/exercicio2/index.ts
@@ -0,0 +1,24 @@
+function adicao(a: number, b: number): number {
+    return a + b
+}
+    const resultAdicao = adicao(7, 3);
+    console.log(resultAdicao);
+
+function subtracao (a: number, b: number): number {
+    return a - b
+}
+    const resultSubtracao = subtracao(9, 4);
+    console.log(resultSubtracao);
+
+function multiplicacao(a: number, b: number): number {
+    return a * b
+}
+    const resultMultiplicacao = multiplicacao(3, 5)
+    console.log(resultMultiplicacao);
+
+function maiorQue(a: number, b: number): number {
+    return a > b ? a : b
+}
+    const resultMaiorQue = maiorQue (12, 5)
+    console.log(resultMaiorQue);
+
